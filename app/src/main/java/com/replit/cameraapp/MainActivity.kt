103346effb7ec3261060dfc02f24f94d76df14e7
Wejora6140 <62113722-wejora6140@users.noreply.replit.com>
2026-07-12 21:27:49 +0000

    Fix graphicsLayer import package

--- app/src/main/java/com/replit/cameraapp/MainActivity.kt
+++ app/src/main/java/com/replit/cameraapp/MainActivity.kt
@@ -99,7 +99,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.graphicsLayer
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
@@ -107,6 +106,7 @@ import androidx.compose.ui.graphics.CompositingStrategy
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.RenderEffect
 import androidx.compose.ui.graphics.asComposeRenderEffect
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
